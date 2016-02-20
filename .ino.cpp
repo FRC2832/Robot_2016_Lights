d411f1f771bef4f5354b156215cd3f30a296e2a6
Justin <justinmarentette11@gmail.com>
2016-02-20 13:53:49 -0500

    Merge... kinda?

--- .ino.cpp
+++ .ino.cpp
@@ -1,14 +1,14 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-01-30 13:59:18
+//This file has been generated on 2016-02-20 13:05:08
 
 #include "Arduino.h"
 #include <Adafruit_NeoPixel.h>
 void setup() ;
 void loop() ;
-void pattern0(int colorMax, int color, int speeed);
-void pattern1(int value);
+void pattern0(int colorMax, int color, int speeed) ;
+void pattern1(int value) ;
 uint32_t Wheel(byte WheelPos) ;
 
 #include "Arduino.ino"
